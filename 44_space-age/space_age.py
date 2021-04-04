6b2631ba8fe8d8ac9a5350839d09c45f8bdbd366
Rohnak Agarwal <rrka79wal@gmail.com>
2021-04-04 10:12:33 +0530

    space_age

---
+++ 44_space-age/space_age.py
@@ -0,0 +1,30 @@
+year_on_earth = 31557600
+
+
+class SpaceAge:
+    def __init__(self, seconds):
+        self.yoe = seconds / year_on_earth
+
+    def on_earth(self):
+        return round(self.yoe, 2)
+
+    def on_mercury(self):
+        return round(self.yoe / 0.2408467, 2)
+
+    def on_venus(self):
+        return round(self.yoe /  0.61519726, 2)
+
+    def on_mars(self):
+        return round(self.yoe / 1.8808158, 2)
+
+    def on_jupiter(self):
+        return round(self.yoe / 11.862615, 2)
+
+    def on_saturn(self):
+        return round(self.yoe / 29.447498, 2)
+
+    def on_uranus(self):
+        return round(self.yoe / 84.016846, 2)
+
+    def on_neptune(self):
+        return round(self.yoe / 164.79132, 2)
