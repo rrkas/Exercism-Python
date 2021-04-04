a679ce3292e8723ba08a7b0cc49cc6d7b46d61c9
Rohnak Agarwal <rrka79wal@gmail.com>
2021-04-04 10:22:25 +0530

    grains

--- grains/grains.py
+++
@@ -1,8 +0,0 @@
-def square(number):
-    if number < 1 or number > 64:
-        raise ValueError('ValueError')
-    return 2 ** (number - 1)
-
-
-def total():
-    return sum([square(i) for i in range(1, 65)])
